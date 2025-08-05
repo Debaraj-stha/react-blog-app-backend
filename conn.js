df1b95a86f799fba3ee9283fd11adf15d8620657
Debaraj-stha <dstha221@gmail.com>
2025-08-05 14:09:47 +0545

    fixing dbms connection

--- conn.js
+++ conn.js
@@ -1,8 +1,9 @@
 const mongoose=require("mongoose")
 const connString=process.env.MONGODB_URL
+let url="mongodb+srv://jeevanstha989:eCcGUVQIe4PgYKZU@cluster0.elgbq5e.mongodb.net/"
 
 
-const conn=mongoose.connect(connString).then((_)=>{
+const conn=mongoose.connect(url).then((_)=>{
     console.log("successfully connected to database")
 }).catch((e)=>{
     console.log("error connetcing to database",e)
